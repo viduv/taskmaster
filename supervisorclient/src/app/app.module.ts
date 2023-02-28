55f22e5701ff635b6c52e6c0f00222e3d5077ab8
VictorD <victor.duvernois2@gmail.com>
2023-02-28 11:17:32 +0100

    Set Up module

---
+++ supervisorclient/src/app/app.module.ts
@@ -0,0 +1,15 @@
+// imports
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+// @NgModule decorator with its metadata
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
